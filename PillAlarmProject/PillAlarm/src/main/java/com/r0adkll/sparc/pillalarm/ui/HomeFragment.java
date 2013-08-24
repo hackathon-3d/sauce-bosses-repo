dbc6d6db5fd0dab1f34db9392d2ba1ee3e3eee27
r0adkll <veedubusc@gmail.com>
2013-08-24 00:00:24 -0400

    Integrated my deadSkunk library

--- PillAlarmProject/PillAlarm/src/main/java/com/r0adkll/sparc/pillalarm/ui/HomeFragment.java
+++ PillAlarmProject/PillAlarm/src/main/java/com/r0adkll/sparc/pillalarm/ui/HomeFragment.java
@@ -1,7 +1,66 @@
 package com.r0adkll.sparc.pillalarm.ui;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by r0adkll on 8/23/13.
  */
-public class HomeFragment {
+public class HomeFragment extends Fragment{
+
+    /*******************************************
+     *
+     * Static Initializers
+     *
+     */
+
+
+    /*******************************************
+     *
+     * Variables
+     *
+     */
+
+
+    /*******************************************
+     *
+     * Lifecycle methods
+     *
+     */
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Load Views
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
